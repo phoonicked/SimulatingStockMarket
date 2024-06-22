ebd256472f4aaf5647ed63f64276b02dc3edfb77
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-06-22 17:16:49 +0100

    Fixed ECMAScript error and separated styles to another file

--- stock-simulation/app/index.tsx
+++ stock-simulation/app/index.tsx
@@ -2,6 +2,7 @@ import {Text, View, StyleSheet, ActivityIndicator, SafeAreaView, Button, TextInp
 import React, { useState, useEffect } from 'react';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { getDailyOpenCloseData } from "@/app/services/polygon-service";
+import {styles} from "@/styles";
 
 const Index = () => {
     const [stockData, setStockData] = useState<any>(null);
@@ -99,50 +100,5 @@ const Index = () => {
         </SafeAreaView>
     );
 };
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#1c1c1c',
-        padding: 20,
-    },
-    topSection: {
-        flex: 1,
-        backgroundColor: '#292929',
-        borderRadius: 16,
-        marginBottom: 20,
-    },
-    card: {
-        backgroundColor: '#292929',
-        borderRadius: 16,
-        padding: 20,
-        marginBottom: 20,
-    },
-    sectionTitle: {
-        color: '#ffffff',
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    input: {
-        backgroundColor: '#333333',
-        color: '#ffffff',
-        borderRadius: 8,
-        padding: 10,
-        marginBottom: 10,
-    },
-    symbol: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: '#ffffff',
-    },
-    text: {
-        fontSize: 16,
-        color: '#ffffff',
-    },
-    errorText: {
-        color: 'red',
-    },
-});
 
 export default Index;
