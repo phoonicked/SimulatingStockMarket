793754e4b033ff9ad23eb8246f7e897cbc7f10bf
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-06-22 16:39:21 +0100

    Added UI components to the app

--- stock-simulation/app/_layout.tsx
+++ stock-simulation/app/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import {DarkTheme, ThemeProvider} from "@react-navigation/native";
+import {theme} from "@/theme";
 
 export default function RootLayout() {
   return (
-      <ThemeProvider value={DarkTheme}>
-          <Stack>
-              <Stack.Screen name="index" />
-          </Stack>
-      </ThemeProvider>
+    <PaperProvider theme = {theme}>
+        <ThemeProvider value={DarkTheme}>
+            <Stack>
+                <Stack.Screen name="index" />
+            </Stack>
+        </ThemeProvider>
+    </PaperProvider>
   );
 }
