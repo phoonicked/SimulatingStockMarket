7549422cb7ad35b7c0c1a222335a238d71d5850b
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-06-22 16:38:25 +0100

    Added UI components to the app

--- stock-simulation/app/_layout.tsx
+++ stock-simulation/app/_layout.tsx
@@ -1,16 +1,13 @@
 import { Stack } from "expo-router";
 import { PaperProvider } from "react-native-paper";
 import {DarkTheme, ThemeProvider} from "@react-navigation/native";
-import { theme } from "@/theme";
 
 export default function RootLayout() {
   return (
-    <PaperProvider theme = {theme}>
-        <ThemeProvider value={DarkTheme}>
-            <Stack>
-                <Stack.Screen name="index" />
-            </Stack>
-        </ThemeProvider>
-    </PaperProvider>
+      <ThemeProvider value={DarkTheme}>
+          <Stack>
+              <Stack.Screen name="index" />
+          </Stack>
+      </ThemeProvider>
   );
 }
--- stock-simulation/app/index.tsx
+++ stock-simulation/app/index.tsx
@@ -1,4 +1,4 @@
-import {Text, View, StyleSheet, ActivityIndicator, SafeAreaView, Button, TextInput} from "react-native";
+import {Text, View, StyleSheet, ActivityIndicator, SafeAreaView, Button, TextInput, TouchableOpacity } from "react-native";
 import React, { useState, useEffect } from 'react';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { getDailyOpenCloseData } from "@/app/services/polygon-service";
@@ -47,40 +47,55 @@ const Index = () => {
 
     return (
         <SafeAreaView style={styles.container}>
-            <TextInput
-                style={styles.input}
-                placeholder={"Enter Stock Ticker"}
-                value={stockTicker}
-                onChangeText={handleTickerChange}
-            />
-            {loading ? (
-                <Text style={styles.text}>Loading...</Text>
-            ) : error ? (
-                <Text style={styles.errorText}>Error fetching data: {error.message}</Text>
-            ) : stockData ? (
-                <View>
-                    <Text style={styles.symbol}>Symbol: {stockData.symbol}</Text>
-                    <Text style={styles.text}>Date: {stockData.from}</Text>
-                    <Text style={styles.text}>Open: {stockData.open}</Text>
-                    <Text style={styles.text}>High: {stockData.high}</Text>
-                    <Text style={styles.text}>Low: {stockData.low}</Text>
-                    <Text style={styles.text}>Close: {stockData.close}</Text>
-                    <Text style={styles.text}>Volume: {stockData.volume}</Text>
-                    <Text style={styles.text}>After Hours: {stockData.afterHours}</Text>
-                    <Text style={styles.text}>Pre Market: {stockData.preMarket}</Text>
-                </View>
-            ) : (
-                <Text style={styles.text}>No data available. Please select a date.</Text>
-            )}
-            <Button title="Select Date" onPress={() => setDatePickerVisibility(true)} />
-            <DateTimePickerModal
-                isVisible={datePickerVisibility}
-                mode="date"
-                onConfirm={handleConfirm}
-                onCancel={() => setDatePickerVisibility(false)}
-                maximumDate={currentDate}
-                minimumDate={oneYearAgo}
-            />
+            {/* Top Section */}
+            <View style={styles.topSection}></View>
+
+            {/* Daily Stock Data Section */}
+            <View style={styles.card}>
+                <Text style={styles.sectionTitle}>Daily Stock Data</Text>
+                <TextInput
+                    style={styles.input}
+                    placeholder={"Enter Stock Ticker"}
+                    value={stockTicker}
+                    onChangeText={handleTickerChange}
+                />
+                <TouchableOpacity onPress={() => setDatePickerVisibility(true)}>
+                    <Text style={styles.input}>{selectedDate ? selectedDate.toDateString() : 'Select Date'}</Text>
+                </TouchableOpacity>
+                {loading ? (
+                    <Text style={styles.text}>Loading...</Text>
+                ) : error ? (
+                    <Text style={styles.errorText}>Error fetching data: {error.message}</Text>
+                ) : stockData ? (
+                    <View>
+                        <Text style={styles.symbol}>Symbol: {stockData.symbol}</Text>
+                        <Text style={styles.text}>Date: {stockData.from}</Text>
+                        <Text style={styles.text}>Open: {stockData.open}</Text>
+                        <Text style={styles.text}>High: {stockData.high}</Text>
+                        <Text style={styles.text}>Low: {stockData.low}</Text>
+                        <Text style={styles.text}>Close: {stockData.close}</Text>
+                        <Text style={styles.text}>Volume: {stockData.volume}</Text>
+                        <Text style={styles.text}>After Hours: {stockData.afterHours}</Text>
+                        <Text style={styles.text}>Pre Market: {stockData.preMarket}</Text>
+                    </View>
+                ) : (
+                    <Text style={styles.text}>No data available. Please select a date.</Text>
+                )}
+                <DateTimePickerModal
+                    isVisible={datePickerVisibility}
+                    mode="date"
+                    onConfirm={handleConfirm}
+                    onCancel={() => setDatePickerVisibility(false)}
+                    maximumDate={currentDate}
+                    minimumDate={oneYearAgo}
+                />
+            </View>
+
+            {/* News Section */}
+            <View style={styles.card}>
+                <Text style={styles.sectionTitle}>News</Text>
+                {/* Placeholder for future content */}
+            </View>
         </SafeAreaView>
     );
 };
@@ -88,26 +103,42 @@ const Index = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundColor: '#1c1c1c',
         padding: 20,
     },
-    input: {
-        height: 40,
-        borderColor: 'gray',
-        borderWidth: 1,
-        paddingHorizontal: 10,
+    topSection: {
+        flex: 1,
+        backgroundColor: '#292929',
+        borderRadius: 16,
         marginBottom: 20,
-        color: '#fff',
+    },
+    card: {
+        backgroundColor: '#292929',
+        borderRadius: 16,
+        padding: 20,
+        marginBottom: 20,
+    },
+    sectionTitle: {
+        color: '#ffffff',
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 10,
+    },
+    input: {
+        backgroundColor: '#333333',
+        color: '#ffffff',
+        borderRadius: 8,
+        padding: 10,
+        marginBottom: 10,
     },
     symbol: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: '#fff', // Set text color to white
+        color: '#ffffff',
     },
     text: {
         fontSize: 16,
-        color: '#fff', // Set text color to white
+        color: '#ffffff',
     },
     errorText: {
         color: 'red',
