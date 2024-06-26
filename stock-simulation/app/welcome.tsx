40d575a02c545310360b141449237be6c94d34a2
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-06-26 23:12:49 +0100

    Beho asked me to remove the exclamation mark

--- stock-simulation/app/welcome.tsx
+++ stock-simulation/app/welcome.tsx
@@ -8,7 +8,7 @@ const welcomeScreen = ({ navigation }: any) => {
             <View style={styles.centerContent}>
                 <Image source={require('../assets/images/logo.png')} style={styles.logo} />
                 <Text style={styles.appTitle}>Oppie</Text>
-                <Text style={styles.slogan}>All you need for market research!</Text>
+                <Text style={styles.slogan}>All you need for market research</Text>
             </View>
             <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('SignIn')}>
                 <Text style={styles.buttonText}>Sign in</Text>
@@ -16,8 +16,12 @@ const welcomeScreen = ({ navigation }: any) => {
             <TouchableOpacity style={[styles.button, styles.createButton]} onPress={() => navigation.navigate('SignUp')}>
                 <Text style={styles.createButtonText}>Create an account</Text>
             </TouchableOpacity>
-            <Text style={styles.orText}>or continue with</Text>
-            <View style={styles.socialContainer}>
+            <View style={styles.orTextContainer}>
+                <View style={styles.orTextLine} />
+                <Text style={styles.orText}>or continue with</Text>
+                <View style={styles.orTextLine} />
+            </View>
+        <View style={styles.socialContainer}>
                 <TouchableOpacity style={styles.socialButton}>
                     <Image source={require('../assets/images/apple.png')} style={styles.socialIcon} />
                 </TouchableOpacity>
