35262f92754c99759b7fc81aa4783ed7f4cd4422
Wilbert Tham <94099663+phoonicked@users.noreply.github.com>
2024-06-27 19:41:59 +0100

    General cleanup

--- stock-simulation/app/_layout.tsx
+++ stock-simulation/app/_layout.tsx
@@ -28,13 +28,13 @@ const AppNavigator = () => {
                 tabBarIcon: ({ focused, color, size }) => {
                     let iconName: keyof typeof Ionicons.glyphMap | undefined;
 
-                    if (route.name === 'index') {
+                    if (route.name === 'screens/index') {
                         iconName = focused ? 'home' : 'home-outline';
-                    } else if (route.name === 'news') {
+                    } else if (route.name === 'screens/news') {
                         iconName = focused ? 'newspaper' : 'newspaper-outline';
-                    } else if (route.name === 'settings') {
+                    } else if (route.name === 'screens/settings') {
                         iconName = focused ? 'settings' : 'settings-outline';
-                    } else if (route.name === 'portfolio') {
+                    } else if (route.name === 'screens/portfolio') {
                         iconName = focused ? 'pie-chart' : 'pie-chart-outline';
                     }
 
@@ -48,10 +48,10 @@ const AppNavigator = () => {
                 },
             })}
         >
-            <Tabs.Screen name="index" options={{ title: 'Home' }} />
-            <Tabs.Screen name="news" options={{ title: 'News' }} />
-            <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
-            <Tabs.Screen name="portfolio" options={{ title: 'Portfolio' }} />
+            <Tabs.Screen name="screens/index" options={{ title: 'Home' }} />
+            <Tabs.Screen name="screens/news" options={{ title: 'News' }} />
+            <Tabs.Screen name="screens/settings" options={{ title: 'Settings' }} />
+            <Tabs.Screen name="screens/portfolio" options={{ title: 'Portfolio' }} />
         </Tabs>
     );
 };
